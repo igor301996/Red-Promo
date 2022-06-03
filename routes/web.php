03d6b8f73fd4df7536ceee9f18683fac733a543c
igor301996 <drashencko.igor@yandex.ru>
2022-06-03 11:25:26 +0300

    base laravel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::prefix('test')->middleware('auth')->group(function (){
+    Route::get('/', function () {
+        return view('welcome');
+    });
+});
 Route::get('/', function () {
     return view('welcome');
 });
